a08f97636bffbadbb758bc66319b2521b1836837
RuM <18311071+ru44@users.noreply.github.com>
2022-09-16 02:07:19 +0300

    Co-authored-by: Я♡M...∞ <18311071+ru44@users.noreply.github.com>

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
 // custom error handler
 app.use((err, req, res, next) => {
     console.error(err.stack)
-    res.status(500).send('its the Server fault 500!');
+    res.status(500).send('its the Server fault 500! ');
 });
 
 app.disable('x-powered-by');
