a04027a87f7dda88be092a458be87fd8bb69cc0d
IFox99 <oabad.1999@gmail.com>
2022-09-15 18:31:08 -0400

    fix 404 error msg

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use('/api/votes', votesRouter);
 
 // custom 404
 app.use((req, res, next) => {
-    res.status(404).send("Sorry can't find that!");
+    res.status(404).send("Page not found 404!");
 });
 
 // custom error handler
