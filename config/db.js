9c7b667d29c5b52e2206d810a87f847dc70903ba
AlBaraa Habiballah <al.habiballah@tamkeentech.sa>
2022-09-27 13:36:55 +0300

    add bcryptjs

--- config/db.js
+++ config/db.js
@@ -4,6 +4,6 @@ const betterSqlite3 = require('better-sqlite3');
 
 const databasePath = path.join(__dirname, "../database", "myDatabase.db");
 
-const db = new betterSqlite3(databasePath, { verbose: console.log("Connnected to database.") });
+const db = new betterSqlite3(databasePath, { verbose: console.log });
 
 module.exports = db;
