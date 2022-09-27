b1a9d514e5b14baa9e7eeba0adf24be7c545ca93
Я♡M...∞ <18311071+ru44@users.noreply.github.com>
2022-09-27 08:40:21 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 const betterSqlite3 = require('better-sqlite3');
 
-// Database
 const databasePath = path.join(__dirname, "../database", "myDatabase.db");
 
 const db = new betterSqlite3(databasePath, { verbose: console.log("Connnected to database.") });
