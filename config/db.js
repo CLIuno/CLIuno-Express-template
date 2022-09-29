3ce428c71bbfdd1f18920edeead2b918bf4b39c0
ru44 <rmahxhook@gmail.com>
2022-09-29 10:27:15 +0300

    .

--- config/db.js
+++ config/db.js
@@ -1,9 +1,4 @@
 const path = require('path');
 
-const betterSqlite3 = require('better-sqlite3');
 
-const databasePath = path.join(__dirname, "../database", "myDatabase.db");
 
-const db = new betterSqlite3(databasePath, { verbose: console.log });
-
-module.exports = db;
